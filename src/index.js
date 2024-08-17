1ae97c0f812cd4d03657d1e57fe0e8f784937e97
Kris Mendoza <thekittengallery@gmail.com>
2024-08-17 23:57:34 +0800

    Render content using JS instead of hardcoding it to HTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import { homepageRender } from './homepage';
 import './styles/styles.css';
 import mapImg from './assets/map.gif';
+
+homepageRender();
 
 const map = document.getElementById('map-image');
 map.src = mapImg;
