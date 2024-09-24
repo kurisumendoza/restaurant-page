d804a7e220091cadf52d13e7ea872c976cf76605
Kris Mendoza <thekittengallery@gmail.com>
2024-09-25 00:31:21 +0800

    Split Webpack config into separate prod and dev files

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-  },
-  devtool: 'source-map',
-  devServer: {
-    static: './dist',
   },
   module: {
     rules: [
